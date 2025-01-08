ba405da4e531ee12aa64ad76a13b5520b955542b
kourpidis_paris <kourpidisparis@gmail.com>
2025-01-08 09:02:06 +0200

    Setup EF Core

--- obj/Debug/net8.0/BulkyWebRazor_Temp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BulkyWebRazor_Temp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWebRazor_Temp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f915309d328e4d9418a137dd2be913b2babcdbd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f22d62220ef0afd1d97333707b71309689b4a6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWebRazor_Temp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWebRazor_Temp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
